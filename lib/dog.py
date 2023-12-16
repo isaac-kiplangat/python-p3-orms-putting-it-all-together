e57dad483cfd985f37735e179530c9525bfffe5e
5000vw <isaackiplangat1999@gmail.com>
2023-12-16 17:31:52 +0300

    updated assignment

--- lib/dog.py
+++ lib/dog.py
@@ -5,4 +5,42 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    pass
+    def __init__(self, name, breed):
+      self.name = name
+      self.breed = breed
+      self._id = None
+      
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        
+    @classmethod
+    def drop_table(self):
+        sql = """DROP TABLE IF EXISTS dogs """
+
+        CURSOR.execute(sql)
+        
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name,breed)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed))
+     
+    @classmethod   
+    def create(cls, name, breed):
+      new_dog = cls(name, breed)
+      new_dog.save()
+      
+      return new_dog
+    
+
+Dog.create("joey", "cocker spaniel")
